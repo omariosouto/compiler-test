f66c7c4773dd673b0f0cc287bce8f9cff94b084e
Yuri Matheus <yurimatheusb@hotmail.com>
2018-06-14 18:28:58 -0300

    altera código para o restify

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 require('dotenv').config()
 
-const Express = require('express')
+
+const Restify = require('restify')
 const { spawnSync } = require( 'child_process' )
 const compilerLanguages = require('./compilerLanguages')
-const app = new Express()
 
+const app = Restify.createServer()
+app.use(Restify.plugins.bodyParser({mapParams: true}))
 
-global.payload = {
-    code: `
-    public class Teste {
-        public static void main(String[] args) {
-            System.out.println("Hello PANCO!!!");
-            new Teste3().teste();
-        } 
-    }
+app.post('/', (req,res) => {
+    //const { language, code } = global.payload2
+
+    const language = req.body.language
+    const code = req.body.code
     
-    class Teste3 {
-        public void teste() {
-            System.out.println("Hello fodac do teste3!");
-        } 
-    }`,
-    language: 'java'
-}
-
-global.payload2 = {
-    code: `
-        const msg = 'alo alo w brazil'
-        console.log('JS FTW');
-        console.log(msg);
-    `,
-    language: 'javascript'
-}
-
-app.get('/', (req,res) => {
-    const { language, code } = global.payload2
     const command = spawnSync( 'docker', compilerLanguages.get(language)(code) )
     
-    res.json({
+    res.send({
         output: command.stdout.toString(),
         error: command.stderr.toString()
     })   
