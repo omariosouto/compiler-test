01949d6eeeec501ddf9c1044e2533eb35cc9b805
Mario Souto <soutomarios@gmail.com>
2018-06-14 13:40:05 -0300

    comitao mvp

---
+++ app.js
@@ -0,0 +1,44 @@
+const Express = require('express')
+const { spawnSync } = require( 'child_process' )
+const compilerLanguages = require('./compilerLanguages')
+const app = new Express()
+
+
+global.payload = {
+    code: `
+    public class Teste {
+        public static void main(String[] args) {
+            System.out.println("Hello PANCO!!!");
+            new Teste3().teste();
+        } 
+    }
+    
+    class Teste3 {
+        public void teste() {
+            System.out.println("Hello fodac do teste3!");
+        } 
+    }`,
+    language: 'java'
+}
+
+global.payload2 = {
+    code: `
+        const msg = 'alo alo w brazil'
+        console.log('JS FTW');
+        console.log(msg);
+    `,
+    language: 'javascript'
+}
+
+app.get('/', (req,res) => {
+    const { language, code } = global.payload
+    const command = spawnSync( 'docker', compilerLanguages.get(language)(code) )
+    
+    res.json({
+        output: command.stdout.toString(),
+        error: command.stderr.toString()
+    })   
+
+})
+
+module.exports = app
---
+++ compilerLanguages.js
@@ -0,0 +1,19 @@
+const languages = new Map()
+
+languages.set('javascript', function(code) {     
+    const javascriptDockerCommand = `run -i --rm node node -p`.split(' ')
+    javascriptDockerCommand.push(code)
+    return javascriptDockerCommand
+})
+
+languages.set('java', function(code) {
+    const matches = payload.code.match(/public\s(\w+\s)?[class]{5}\s(\w+)/)
+    const classNameWithMainMethod = matches[2]
+
+    const javaDockerCommand = `run -i --rm -w /app openjdk:9-jdk sh -c`.split(' ')
+    javaDockerCommand.push(`echo ${JSON.stringify(code)} > ${classNameWithMainMethod}.java && javac ${classNameWithMainMethod}.java && java ${classNameWithMainMethod}`)
+    
+    return javaDockerCommand
+})
+
+module.exports = languages
