8050d68b522b7ef5a06cc7046f5a8e01aed915d4
Mario Souto <soutomarios@gmail.com>
2018-06-15 14:55:04 -0300

    deus no comando

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 
 const Express = require('express')
-const { spawnSync } = require( 'child_process' )
+const { spawn, spawnSync } = require( 'child_process' )
 const compilerLanguages = require('./compilerLanguages')
+const terminate = require('terminate')
 const app = new Express()
 
 
@@ -28,18 +29,71 @@ global.payload2 = {
         const msg = 'alo alo w brazil'
         console.log('JS FTW');
         console.log(msg);
+        while(true) {
+            console.log('hi')
+        }
     `,
     language: 'javascript'
 }
 
-app.get('/', (req,res) => {
-    const { language, code } = global.payload2
-    const command = spawnSync( 'docker', compilerLanguages.get(language)(code) )
+
     
-    res.json({
-        output: command.stdout.toString(),
-        error: command.stderr.toString()
-    })   
+
+    // const command = spawn( 'docker', compilerLanguages.get(language)(code) )
+    
+    // command.stdout.on('data', (data) => {
+    //     spawnSync('docker', 'rm --force node1'.split(' '))
+    //     console.log('output', data)
+    // });
+    
+    // command.stderr.on('data', (data) => {
+        //     console.log('error', data)
+    // });
+
+global.counter = 0
+
+app.get('/', (req,res) => {
+    let counter = global.counter++
+    console.log(counter, 'Request');
+    const { language, code } = global.payload2
+
+    const containerId = Math.floor(Math.random() * 1000)
+
+
+    console.log(`${counter} - Rodando o container ${containerId}`)
+    const child = spawn( 'docker', compilerLanguages.get(language)(code, containerId), {
+        detached: true
+    })
+
+    // Pegando o Output e os Erros
+    let resOutput = 'output'
+    let resError = 'error'
+    // Limitar a 10: child.stdout.on('data', (data) => resOutput += data);    
+    // Limitar a 10: child.stderr.on('data', (data) => resError += data);
+    
+    // Tempo limite de execução
+    const limitTime = 5 * 1000
+    const to = setTimeout(function(){
+        console.log(`${counter} - Ordenando matar o processo`);
+        terminate(child.pid, function (err) {
+            if (!err) {
+                console.log('done'); // terminating the Processes succeeded.
+                spawn('docker', `rm --force node${containerId}`.split(' '))
+                    .on('exit', () => console.log(`container morto: ${containerId}`))
+
+            }
+          });
+          
+    }, limitTime);  
+    
+    child.on('exit', function(){
+        clearTimeout(to); // if finish okay, don't kill docker container, because it's finished okay
+        console.log(`${counter} - Processo morto!`);
+        res.json({
+            resOutput,
+            resError
+        })
+    });
 
 })
 
--- compilerLanguages.js
+++ compilerLanguages.js
@@ -1,16 +1,16 @@
 const languages = new Map()
 
-languages.set('javascript', function(code) {     
-    const javascriptDockerCommand = `run -i --rm node node -p`.split(' ')
+languages.set('javascript', function(code, containerId) {     
+    const javascriptDockerCommand = `run --name node${containerId} -i --rm node node -p`.split(' ')
     javascriptDockerCommand.push(code)
     return javascriptDockerCommand
 })
 
-languages.set('java', function(code) {
+languages.set('java', function(code, containerId) {
     const matches = payload.code.match(/[public]{6}\s(\w+\s)?[class]{5}\s(\w+)/)
     const classNameWithMainMethod = matches[2]
 
-    const javaDockerCommand = `run -i --rm -w /app openjdk:9-jdk sh -c`.split(' ')
+    const javaDockerCommand = `run --name java${containerId} -i --rm -w /app openjdk:9-jdk sh -c`.split(' ')
     javaDockerCommand.push(`echo ${JSON.stringify(code)} > ${classNameWithMainMethod}.java && javac ${classNameWithMainMethod}.java && java ${classNameWithMainMethod}`)
     
     return javaDockerCommand
