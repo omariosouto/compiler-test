d3b94c7cd3b8c3bc9a77414fa944bc925d572f83
Yuri Matheus <yurimatheusb@hotmail.com>
2018-06-19 11:54:25 -0300

    Adicionada as configurações de hardware

--- compilerLanguages.js
+++ compilerLanguages.js
@@ -1,7 +1,12 @@
 const languages = new Map()
 
+
+const memory = '512m'
+const cpus = '1'
+const hardwareConfig = `--memory=${memory} --cpus=${cpus}`
+
 languages.set('javascript', function(code) {     
-    const javascriptDockerCommand = `run -i --rm node node -p`.split(' ')
+    const javascriptDockerCommand = `run -i ${hardwareConfig} --rm node node -p`.split(' ')
     javascriptDockerCommand.push(code)
     return javascriptDockerCommand
 })
@@ -10,7 +15,7 @@ languages.set('java', function(code) {
     const matches = code.match(/[public]{6}\s(\w+\s)?[class]{5}\s(\w+)/)
     const classNameWithMainMethod = matches[2]
 
-    const javaDockerCommand = `run -i --rm -w /app openjdk:9-jdk sh -c`.split(' ')
+    const javaDockerCommand = `run -i ${hardwareConfig} --rm -w /app openjdk:9-jdk sh -c`.split(' ')
     javaDockerCommand.push(`echo ${JSON.stringify(code)} > ${classNameWithMainMethod}.java && javac ${classNameWithMainMethod}.java && java ${classNameWithMainMethod}`)
     
     return javaDockerCommand
