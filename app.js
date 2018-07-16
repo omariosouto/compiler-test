d13ae9ba42b400fd036348e7d0f8087b402a7b59
Yuri Matheus <yurimatheusb@hotmail.com>
2018-07-16 13:27:12 -0300

    remove os comentários

--- app.js
+++ app.js
@@ -20,20 +20,16 @@ app.post('/', (req,res) => {
     const language = req.body.language
     const code = req.body.code
   
-    // 0 - Gerar ID
     const containerId = Math.floor(Math.random() * 1000)
     
 
-    // 1 - Iniciando o container
     console.log(`${counter} - Rodando o container ${containerId}`)
     const processCompiler = spawn( 'docker', compilerLanguages.get(language)(code, containerId), {
         detached: true
     })
     
-    // 1.1 - Código para matar o processo atual
     let processCompilerFinishing = false
     PubSub.subscribe('PROCESSCOMPILER_AND_DOCKERCONTAINER_KILLER', (channel, msg) => {
-        // console.log(`[SHOULD KILL EVERY THING! -[[${msg}]]- ]`)
         if(!processCompilerFinishing) {
             processCompilerStatus = msg
             processCompilerFinishing = true
@@ -47,7 +43,6 @@ app.post('/', (req,res) => {
         }
     })
 
-    // 2 - Gerenciando Outputs e erros do processo que subiu o container
     let processCompilerOutput = '', processCompilerError = '', processCompilerStatus = ''
     let firstOutput = true
     processCompiler.stdout.on('data', (data) => { 
@@ -55,10 +50,8 @@ app.post('/', (req,res) => {
             PubSub.publish('PROCESSCOMPILER_AND_DOCKERCONTAINER_START_TIMEOUT')
             firstOutput = false;
         } else if(processCompilerOutput.toString().length > 500) {
-            // console.log(`Matar o container`)
             PubSub.publish('PROCESSCOMPILER_AND_DOCKERCONTAINER_KILLER', 'OutPut Too Long')
         } else {
-            // console.log('Incrementa output')
             processCompilerOutput += data.toString()
         }
 
@@ -68,7 +61,6 @@ app.post('/', (req,res) => {
         processCompilerError += data
     }); 
 
-    // 3 - Iniciando TimeOut para o Container
     const containerExecTimeLimit = 10 * 1000
     let containerTimeOut 
     PubSub.subscribe('PROCESSCOMPILER_AND_DOCKERCONTAINER_START_TIMEOUT', () => {
@@ -77,12 +69,10 @@ app.post('/', (req,res) => {
         }, containerExecTimeLimit)    
     })
     
-    // 4 - Saida do processo que subiu o container
+
     processCompiler.on('exit', function(){
         clearTimeout(containerTimeOut); 
-        // console.log(`[processCompiler:${containerId}] Processo que iniciou o container ${containerId} foi morto!`);
-        // console.log(`[processCompiler:${containerId}] processCompilerOutput:`, processCompilerOutput)
-        // console.log(`[processCompiler:${containerId}] processCompilerError:`, processCompilerError)
+
         res.send({
             processCompilerOutput,
             processCompilerError,
